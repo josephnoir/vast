56e9bbbd3e0420149866e6e91167c8370ef05228
Philipp Meyer <philipp.meyer@haw-hamburg.de>
2017-05-31 17:59:04 +0200

    Add new MRT format files

---
+++ libvast/src/format/mrt.cpp
@@ -0,0 +1,122 @@
+#include "vast/error.hpp"
+
+#include "vast/format/mrt.hpp"
+
+namespace vast {
+namespace format {
+namespace mrt {
+
+mrt_parser::mrt_parser() {
+  // Announce type.
+  auto fields = std::vector<record_field>{
+    {"timestamp", timestamp_type{}},
+    {"source_ip", address_type{}},
+    {"source_as", count_type{}},
+    {"prefix", subnet_type{}},
+    {"as_path", vector_type{count_type{}}},
+    {"origin_as", count_type{}},
+    {"origin", string_type{}},
+    {"nexthop", address_type{}},
+    {"local_pref", count_type{}},
+    {"med", count_type{}},
+    {"community", string_type{}},
+    {"atomic_aggregate", string_type{}},
+    {"aggregator", string_type{}},
+  };
+  mrt_bgp4mp_announce_type = record_type{fields};
+  mrt_bgp4mp_announce_type.name("mrt::bgp4mp::announcement");
+  // Table dump type.
+  mrt_table_dump_type = record_type{std::move(fields)};
+  mrt_table_dump_type.name("mrt::table_dump");
+  // Withdraw type
+  auto withdraw_fields = std::vector<record_field>{
+    {"timestamp", timestamp_type{}},
+    {"source_ip", address_type{}},
+    {"source_as", count_type{}},
+    {"prefix", subnet_type{}},
+  };
+  mrt_bgp4mp_withdraw_type = record_type{std::move(withdraw_fields)};
+  mrt_bgp4mp_withdraw_type.name("mrt::bgp4mp::withdrawn");
+  // State-change type.
+  auto state_change_fields = std::vector<record_field>{
+    {"timestamp", timestamp_type{}},
+    {"source_ip", address_type{}},
+    {"source_as", count_type{}},
+    {"old_state", string_type{}},
+    {"new_state", string_type{}},
+  };
+  mrt_bgp4mp_state_change_type = record_type{std::move(state_change_fields)};
+  mrt_bgp4mp_state_change_type.name("mrt::bgp4mp::state_change");
+  // Open type.
+  auto open_fields = std::vector<record_field>{
+    {"timestamp", timestamp_type{}},
+    {"version", count_type{}},
+    {"my_autonomous_system", count_type{}},
+    {"hold_time", count_type{}},
+    {"bgp_identifier", count_type{}},
+  };
+  mrt_bgp4mp_open_type = record_type{std::move(open_fields)};
+  mrt_bgp4mp_open_type.name("mrt::bgp4mp::open");
+  // Notification type.
+  auto notification_fields = std::vector<record_field>{
+    {"timestamp", timestamp_type{}},
+    {"error_code", count_type{}},
+    {"error_subcode", count_type{}},
+  };
+  mrt_bgp4mp_notification_type = record_type{std::move(notification_fields)};
+  mrt_bgp4mp_notification_type.name("mrt::bgp4mp::notification");
+  // Keepalive type.
+  auto keepalive_fields = std::vector<record_field>{
+      {"timestamp", timestamp_type{}},
+  };
+  mrt_bgp4mp_keepalive_type = record_type{std::move(keepalive_fields)};
+  mrt_bgp4mp_keepalive_type.name("mrt::bgp4mp::keepalive");
+}
+
+reader::reader(std::unique_ptr<std::istream> input) : input_{std::move(input)} {
+  VAST_ASSERT(input_);
+}
+
+expected<event> reader::read() {
+  return make_error(ec::unspecified, "Not implemented");
+}
+
+expected<void> reader::schema(vast::schema const& sch) {
+  auto types = {
+    &parser_.mrt_bgp4mp_announce_type,
+    &parser_.mrt_table_dump_type,
+    &parser_.mrt_bgp4mp_withdraw_type,
+    &parser_.mrt_bgp4mp_state_change_type,
+    &parser_.mrt_bgp4mp_open_type,
+    &parser_.mrt_bgp4mp_notification_type,
+    &parser_.mrt_bgp4mp_keepalive_type,
+  };
+  for (auto t : types)
+    if (auto u = sch.find(t->name())) {
+      if (!congruent(*t, *u))
+        return make_error(ec::format_error, "incongruent type:", t->name());
+      else
+        *t = *u;
+    }
+  return {};
+}
+
+expected<schema> reader::schema() const {
+  vast::schema sch;
+  sch.add(parser_.mrt_bgp4mp_announce_type);
+  sch.add(parser_.mrt_table_dump_type);
+  sch.add(parser_.mrt_bgp4mp_withdraw_type);
+  sch.add(parser_.mrt_bgp4mp_state_change_type);
+  sch.add(parser_.mrt_bgp4mp_open_type);
+  sch.add(parser_.mrt_bgp4mp_notification_type);
+  sch.add(parser_.mrt_bgp4mp_keepalive_type);
+  return sch;
+}
+
+char const* reader::name() const {
+  return "mrt-reader";
+}
+
+} // namespace mrt
+} // namespace format
+} // namespace vast
---
+++ libvast/vast/format/mrt.hpp
@@ -0,0 +1,57 @@
+#ifndef VAST_FORMAT_MRT_HPP
+#define VAST_FORMAT_MRT_HPP
+
+#include <iostream>
+
+#include "vast/concept/parseable/core.hpp"
+#include "vast/event.hpp"
+#include "vast/expected.hpp"
+#include "vast/schema.hpp"
+
+namespace vast {
+namespace format {
+namespace mrt {
+
+/// A parser that reading bgp messages from MRT files.
+struct mrt_parser : parser<mrt_parser> {
+  using attribute = event;
+
+  mrt_parser();
+
+  type mrt_bgp4mp_announce_type;
+  type mrt_table_dump_type;
+  type mrt_bgp4mp_withdraw_type;
+  type mrt_bgp4mp_state_change_type;
+  type mrt_bgp4mp_open_type;
+  type mrt_bgp4mp_notification_type;
+  type mrt_bgp4mp_keepalive_type;
+};
+
+/// A MRT reader.
+class reader {
+public:
+  reader() = default;
+
+  /// Constructs a MRT reader.
+  explicit reader(std::unique_ptr<std::istream> input);
+
+  expected<event> read();
+
+  expected<void> schema(vast::schema const& sch);
+
+  expected<vast::schema> schema() const;
+
+  const char* name() const;
+
+private:
+  mrt_parser parser_;
+  std::unique_ptr<std::istream> input_;
+  std::vector<event> event_queue_;
+  event first_event_;
+};
+
+} // namespace mrt
+} // namespace format
+} // namespace vast
+
+#endif
